83c515b270167943b4d3d1ca327b15122eae17a3
Shahd_totah <shahdtotah00@gmail.com>
2025-03-23 22:16:04 +0300

    veritabani olusturma

--- laravel-api/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\Api\V1\BooksController;
-use App\Http\Controllers\Api\V1\WritersController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
